811c1f7795b704d92da9819106c964ba31ff80ac
Wilfrid Joseph Landry <landrywj@me.com>
2016-05-06 16:50:05 -0700

    update deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,9 @@ set :repo_url, 'git@github.com:landrywj/edify'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/landrywj/apps/edify'
 
+#Optional restart mechanism for Capistrano-Passenger
+set :passenger_restart_with_touch, true
+
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
@@ -19,6 +22,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :default_env, path: '/opt/ruby/bin:$PATH'
 
 namespace :deploy do
+  
   desc 'Create, migrate and seed the database'
   task :database_setup do
     on roles(:db) do
