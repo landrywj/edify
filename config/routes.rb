67488478a42155396634b51501355a09c48d35cb
Joe Landry <landrywj@me.com>
2018-07-22 15:14:49 -0700

    update to 5.2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,3 @@
 Rails.application.routes.draw do
-  root 'application#newsfeed'
-  devise_for :users
-  get '/accounts' => 'application#accounts'
-  resources :buildings
-  resources :rooms
-  resources :educators
-  resources :semesters
-  resources :offerings
-  resources :courses
-  resources :students
-  resources :comments
-  resources :posts do
-    resources :comments
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
