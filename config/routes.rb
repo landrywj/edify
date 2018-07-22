6277f9c880b633ab357eb9f5b8aa681dd827d909
Joe Landry <landrywj@me.com>
2018-07-22 15:33:28 -0700

    add back routes and a default user in seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#newsfeed'
+  devise_for :users
+  get '/accounts' => 'application#accounts'
+  resources :buildings
+  resources :rooms
+  resources :educators
+  resources :semesters
+  resources :offerings
+  resources :courses
+  resources :students
+  resources :comments
+  resources :posts do
+    resources :comments
+  end
 end
